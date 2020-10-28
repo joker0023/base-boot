533258ec567dda3897025d206f4ecc2beba97e6e
weijie.li <weijie.li@wehotelglobal.com>
2020-10-28 14:47:47 +0800

    添加mybatis-plus

--- src/main/java/com/jokerstation/boot/BootApplication.java
+++ src/main/java/com/jokerstation/boot/BootApplication.java
@@ -1,18 +1,17 @@
 package com.jokerstation.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 //import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//import tk.mybatis.spring.annotation.MapperScan;
-
 @EnableAsync
 //@EnableTransactionManagement
 //@EnableEurekaClient
 @SpringBootApplication
-//@MapperScan("com.jokerstation.common.mapper")
+@MapperScan("com.jokerstation.common.mapper")
 @ComponentScan("com.jokerstation")
 public class BootApplication {
 
